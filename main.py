af6d106fd62a68d49ca337cb418ede0d41b5f41d
foteliasz <58996128+foteliasz@users.noreply.github.com>
2023-01-30 12:13:40 +0100

    Draft of the solution

---
+++ main.py
@@ -0,0 +1,27 @@
+class Func:
+    def __init__(self, name, author, delegate):
+        self.name = name
+        self.author = author
+        self.delegate = delegate
+
+
+def example():
+    print("You are definitely awesome Today ...!")
+    print("Keep it up!")
+
+
+func = Func("example-func", "...", example)
+
+functions = [
+    func
+]
+
+while True:
+    print("Good morning ...")
+    print("List of available functions:")
+    print(type(functions))
+    for index, function in enumerate(functions):
+        print(f'{index + 1}. Function: >{function.name}<, by: {function.author}')
+
+    selection = input("What function would you like to run:")
+    functions[int(selection) - 1].delegate()
